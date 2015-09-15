4804a0dfa5d424abf8b732c1d5ce49c6a5f43323
Marozoff <pavlikmarozofff@gmail.com>
2015-09-15 10:34:13 +0700

    drew
    настройка отрисовки

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace game_git_GL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.game_okno = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // game_okno
+            // 
+            this.game_okno.AccumBits = ((byte)(0));
+            this.game_okno.AutoCheckErrors = false;
+            this.game_okno.AutoFinish = false;
+            this.game_okno.AutoMakeCurrent = true;
+            this.game_okno.AutoSwapBuffers = true;
+            this.game_okno.BackColor = System.Drawing.Color.Black;
+            this.game_okno.ColorBits = ((byte)(32));
+            this.game_okno.DepthBits = ((byte)(16));
+            this.game_okno.Location = new System.Drawing.Point(13, 13);
+            this.game_okno.Name = "game_okno";
+            this.game_okno.Size = new System.Drawing.Size(50, 50);
+            this.game_okno.StencilBits = ((byte)(0));
+            this.game_okno.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.game_okno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl game_okno;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.Platform.Windows;
+using Tao.DevIl;
+
+namespace game_git_GL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            game_okno.InitializeContexts();
+        }
+        anEngin engin;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Incelizacia_gl();
+            engin = new anEngin();
+            timer1.Start();
+        }
+        void Incelizacia_gl()
+        {
+            Glut.glutInit();
+            // инициализация режима окна 
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            // устанавливаем цвет очистки окна 
+            Gl.glClearColor(255, 255, 255, 1);
+            // устанавливаем порт вывода, основываясь на размерах элемента управления AnT 
+            Gl.glViewport(0, 0, game_okno.Width, game_okno.Height);
+            // устанавливаем проекционную матрицу 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очищаем ее 
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(0.0, game_okno.Width, 0.0, game_okno.Height);
+            // переходим к объектно-видовой матрице 
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+        }
+        void drew()
+        {
+            // очистка буфера цвета и буфера глубины 
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            // очищение текущей матрицы 
+            Gl.glLoadIdentity();
+            // установка черного цвета 
+            Gl.glColor3f(0, 0, 0);
+            // визуализация изображения из движка 
+            engin.drew();
+            // дожидаемся завершения визуализации кадра 
+            Gl.glFlush();
+            // сигнал для обновление элемента, реализующего визуализацию. 
+            game_okno.Invalidate();
+            Gl.glFinish();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            drew();
+        }
+    }
+}
